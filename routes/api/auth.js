1e7978e103dc016a924b00b9e06687d36906fa68
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-06-01 23:42:03 +0530

    Authentication is done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const express = require('express')
-const { model } = require('mongoose')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+
+const User = require('../../models/Users')
 
 // @route    GET api/auth
 // @desc     Test route
 // @access   Public 
-router.get('/', (req,res) => res.send('Auth Route'))
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
 
 module.exports = router
