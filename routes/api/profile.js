216720648443d639dd9863d719a88c83149586a3
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-06-02 11:56:37 +0530

    Get user profile as logged in

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,29 @@
 const express = require('express')
-const { model } = require('mongoose')
 const router = express.Router()
+const auth = require('../../middleware/auth')
 
-// @route    GET api/profile
-// @desc     Test route
-// @access   Public 
-router.get('/', (req,res) => res.send('Profile Route'))
+const User = require('../../models/Users')
+const Profile = require('../../models/Profile')
+const { findOne } = require('../../models/Profile')
+
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id })
+            .populate('user', ['name', 'avatar'])
+
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for user' })
+        }
+
+        res.json(profile)
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
 
 module.exports = router
