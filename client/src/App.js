ea71d566d5bcb47c1cad4f2d4db494349bd6deff
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-06-03 15:16:56 +0530

    Redux store is created

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,26 @@ import Navbar from './component/layout/Navbar'
 import Landing from './component/layout/Landing'
 import Login from './component/auth/Login'
 import Register from './component/auth/Register'
-
+//Redux
+import { Provider } from 'react-redux'
+import store from './store'
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
-
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 
 )
 
