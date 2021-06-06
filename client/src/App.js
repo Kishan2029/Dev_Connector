dec444129ab81467bbc996066ec78bb1226da290
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-06-06 17:17:58 +0530

    AddEducation and AddExperience is implemented

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Alert from './component/layout/Alert'
 import Dashboard from './component/dashboard/Dashboard'
 import CreateProfile from './component/profile-form/CreateProfile'
 import EditProfile from './component/profile-form/EditProfile'
+import AddExperience from './component/profile-form/AddExperience'
+import AddEducation from './component/profile-form/AddEducation'
 import PrivateRoute from './component/routing/PrivateRoute'
 //Redux
 import { Provider } from 'react-redux'
@@ -40,6 +42,8 @@ const App = () => {
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+              <PrivateRoute exact path="/add-experience" component={AddExperience} />
+              <PrivateRoute exact path="/add-education" component={AddEducation} />
             </Switch>
           </section>
         </Fragment>
