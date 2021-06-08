2ed4ca959d282a2ef6c75af7c827c41031cc7033
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-06-08 15:43:08 +0530

    PostItem component is created

--- client/src/component/posts/Posts.js
+++ client/src/component/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
+import PostItem from './PostItem'
 import { getPosts } from '../../actions/post'
 
 
@@ -10,9 +11,23 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
         getPosts()
     }, [getPosts])
     return (
-        <div>
+        loading ? <Spinner /> : (
+            <Fragment>
+                <h1 className="large text-primary">Posts</h1>
+                <p className="lead">
+                    <i className="fas fa-user"></i> Welcome to the Community
+                </p>
 
-        </div>
+                {/* Post Form */}
+                <div className="posts">
+                    {
+                        posts.map(post => (
+                            <PostItem key={post._id} post={post} />
+                        ))
+                    }
+                </div>
+            </Fragment>
+        )
     )
 }
 
