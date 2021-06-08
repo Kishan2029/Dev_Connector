b427c1b044cfcc8db9ec2d5bb426895163b7bc51
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-06-08 16:19:44 +0530

    Like and Unlike post is implemented

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { setAlert } from './alert'
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKE
 } from './types'
 
 // Get posts
@@ -22,3 +23,36 @@ export const getPosts = () => async dispatch => {
     }
 }
 
+// Add like
+export const addLike = (id) => async dispatch => {
+    try {
+        const res = await axios.put(`api/posts/like/${id}`)
+
+        dispatch({
+            type: UPDATE_LIKE,
+            payload: { id, likes: res.data }
+        })
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+// Remove like
+export const removeLike = (id) => async dispatch => {
+    try {
+        const res = await axios.put(`api/posts/unlike/${id}`)
+
+        dispatch({
+            type: UPDATE_LIKE,
+            payload: { id, likes: res.data }
+        })
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,6 +1,7 @@
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKE
 } from '../actions/types'
 
 const initialState = {
@@ -22,6 +23,14 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 error: payload,
+                loading: false
+            }
+        case UPDATE_LIKE:
+            return {
+                ...state,
+                posts: state.posts.map(post => post._id === payload.id ?
+                    { ...post, likes: payload.likes } : post
+                ),
                 loading: false
             }
         default:
